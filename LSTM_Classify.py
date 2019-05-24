71445ba67099c7b52d8b8f06802f69be2c5362ca
ankitiiest21 <48135243+ankitiiest21@users.noreply.github.com>
2019-05-24 14:38:10 +0530

    Create LSTM_Classify.py

---
+++ LSTM_Classify.py
@@ -0,0 +1,39 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import glob
+import os
+
+def findFiles(path): return glob.glob(path)
+
+print(findFiles('data/names/*.txt'))
+
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+print('n_letters:',n_letters)
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+print(unicodeToAscii('Ślusàrski'))
+print(unicodeToAscii('Ankit'))
+category_lines = {}  
+all_categories = []
+
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+print('Category Lines: ',category_lines)
+print('All Category: ',all_categories)
+n_categories = len(all_categories)
