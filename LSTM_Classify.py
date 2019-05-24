1b9b69026f29d7891b7bfd64f673bf965840ea38
ankitiiest21 <48135243+ankitiiest21@users.noreply.github.com>
2019-05-24 16:02:28 +0530

    Update LSTM_Classify.py

--- LSTM_Classify.py
+++ LSTM_Classify.py
@@ -211,3 +211,27 @@ for iter in range(0, n_iters + 1):
     plt.show()
 
     
+------------------------------------------ PREDICTING FROM USER INPUT --------------------------------------------------------
+
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        output = evaluate(lineToTensor(input_line))
+
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 2, True)
+        #print(topv.shape)
+        #print(topi.shape)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][0][i].item()
+            category_index = topi[0][0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+predict('Dovesky')
+predict('Jackson')
+predict('Satoshi')
+    
